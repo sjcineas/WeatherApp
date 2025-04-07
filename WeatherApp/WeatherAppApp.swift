3b4b4ca1a2f108e9e2a9050b822348cda884a740
sjcineas <101082903+sjcineas@users.noreply.github.com>
2025-04-07 12:12:43 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Schadrack Cineas on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
